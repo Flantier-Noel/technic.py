d26ce3bcc7d9f144358d0f9dd7420c1c1e86653f
Flantier <noel.flantierGH@gmail.com>
2025-08-04 14:15:26 +0000

    modified:   _default_values.py 	modified:   items.py

--- _default_values.py
+++ _default_values.py
@@ -1,10 +1,20 @@
 # default gears values
 
 _gear_rads = [0.5, 0.9, 1.3, 2.15]
-_gear_tths = [4/21, 5/8, 6/5, 21/6]
+_gear_tths = [8, 16, 24, 40]
 _gear_dict = {'gx0001':(0,0), 'gx0002':(1,1), 'gx0002':(2,2), 'gx0003':(3,3)}
 _wgtGear_fun = lambda rad : 0.825*rad**2 - 0.197*rad + 0.010  ## values explained in doc
 
 # default axes values
 
 _wgtAxe_fun = lambda lgt : 0.152*lgt - 0.039                  ## values explained in doc
+
+## default beam values
+
+_wgtBeam_fun = lambda lgt : 0.274*lgt - 0.219                 ## values explained in doc
+
+## default join values
+
+_wgtJoin_fun = lambda lgt : 0.050*lgt + 0.049
+
+#
--- items.py
+++ items.py
@@ -2,6 +2,16 @@ import math
 import warnings
 import _default_values as dft
 
+class _Hole():
+    ''' basic type for connections between _Items 
+    
+    centerEnt : (float)*3                       < represents the 3d position (x, y, z) of the center of the hole's entry
+    centerExt : (float)*3                       < represents the 3d position (x, y, z) of the center of the hole's exit 
+    from      : str : 'cross' || 'round'        < defined the form of the hole '''
+
+    def __init__(self, centerEnt:(float), centerExt:(float), form:str):
+        pass
+
 class _Items():
     ''' basic type for any objects such as gear, axes, ...
     
@@ -25,7 +35,7 @@ class _Items():
         _default_gears = dft._gear_dict         # {(int, int)} < dict of default gear given by index - keys/values given in doc
 
         def __init__(self, center_pos3d:(float), rad=None, nb_tooth=None, dft_gr=None):
-            wgt = 100
+            wgt = dft._wgtGear_fun(rad)
             _Items.__init__(self, center_pos3d, wgt)
 
             assert (rad != None and nb_tooth != None) or dft_gr != None, "Gear : gear badly defined, either (rad, nb_tooth) or dft_gr must be described"
@@ -46,10 +56,39 @@ class _Items():
         ''' axes child class : axes described by its length
         
             center_pos3d                        < super arg
-            length      : int                   < describes the length of the axes by the number of studs '''
+            length      : int                   < describes the length of the axes by its number of studs '''
         
         def __init__(self, center_pos3d:(float), length:int):
-            wgt = 100
+            wgt = dft._wgtAxe_fun(length)
             _Items.__init__(self, center_pos3d, wgt)
 
             self.lgt = length
+    
+    class Join():
+        ''' join child class :
+
+        center_pos3d                            < super arg
+        length      : int                       < describes the length of the join by its number of studs 
+        rough       : bool                      < describes either the join could rotate or not 
+        form_lst    : ('cross' || 'round') list < describes the formes of each part of the join '''
+
+        def __init__(self, center_pos3d:(float), length:int, rough:bool, form_lst:[str]):
+            wgt = dft._wgtJoin_fun(length)
+            _Items.__init__(self, center_pos3d, wgt)
+            
+            if length > 3 : warnings.warn("Join : unusual length described")
+            assert len(form_lst) == length, "Join : wrong dimension for form_lst"
+
+            self.length = length
+            self.rough = rough
+            self.form_lst = form_lst
+
+    class Beam(): ## gestion de la forme ??
+        ''' beam child class :
+        
+        center_pos3d                        < super arg
+        length      : int                   < describes the length of the beam by its number of studs '''
+
+        def __init__(self,center_pos3d:(float), length:int):
+            wgt = dft._wgtBeam_fun(length)
+            _Items.__init__(self, center_pos3d, wgt)
