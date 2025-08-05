a2d6c03703d03d66e99d93aa8b3625109cf0fbe7
Flantier <noel.flantierGH@gmail.com>
2025-08-05 16:28:28 +0000

    modified:   _passiv3d.py

--- _passiv3d.py
+++ _passiv3d.py
@@ -1,2 +1,20 @@
 import items
-import matplotlib.pyplot as plt
+from math import cos, sin
+#import matplotlib.pyplot as plt
+
+def projection(X:(float), th:float, ph:float):
+    ''' projection 3D into 2D (cf. wiki) 
+
+    X  : (float)*3     < represents the 3d cartesian position of the point to project
+    th : float         < the angle theta = the latitude  of the viewer
+    ph : float         < the angle phi   = the longitude of the viewer '''
+
+    x, y, z = X
+
+    vx, vy     = -sin(ph)       , cos(ph)
+    wx, wy, wz = sin(th)*cos(ph), sin(th)*sin(ph), cos(th)
+
+    x2 = vx*x + vy*y
+    y2 = wx*x + wy*y + wz*z
+
+    return x2, y2
